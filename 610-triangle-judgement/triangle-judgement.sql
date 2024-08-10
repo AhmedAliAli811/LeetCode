0a9b3602deb0187bbb983399caf984cd7c7445a5
Ahmed Ali Ali <81569432+AhmedAliAli811@users.noreply.github.com>
2024-08-10 11:27:28 +0300

    Time: 321 ms (37.80%) | Memory: 0B (100.00%) - LeetSync

--- 610-triangle-judgement/triangle-judgement.sql
+++ 610-triangle-judgement/triangle-judgement.sql
@@ -1,7 +1,9 @@
 /* Write your T-SQL query statement below */
 
 select x,y,z , 
-case when x+y>z and x+z>y and y+z>x 
-then 'Yes' else 'No'
+case 
+when x+y>z and x+z>y and y+z>x 
+then 'Yes' 
+else 'No'
 end Triangle
 from Triangle 
