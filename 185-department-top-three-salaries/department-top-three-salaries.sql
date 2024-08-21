e400ae669b0a9407fdc7b2d957a9f5839d9058ed
Ahmed Ali Ali <81569432+AhmedAliAli811@users.noreply.github.com>
2024-08-21 21:18:39 +0300

    Time: 421 ms (96.91%) | Memory: 0B (100.00%) - LeetSync

---
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -0,0 +1,9 @@
+/* Write your T-SQL query statement below */
+with cte as (
+    select * , dense_rank() over (partition by departmentId order by salary desc) as rank
+    from Employee
+)
+select d.name as Department , c.name as Employee  , c.salary as Salary 
+from Department d join cte c 
+on d.id = c.departmentId 
+where rank < 4
